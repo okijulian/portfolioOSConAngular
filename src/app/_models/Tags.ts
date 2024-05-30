d2c7d2ef7cbaa47bb6af37e59d4296657a12a1c5
Oscar J Sosa Ortiz <okijulian0791@gmail.com>
2024-05-30 19:42:51 -0300

    se agrega los proyectos y se cambian estilos de tags

--- src/app/_models/Tags.ts
+++ src/app/_models/Tags.ts
@@ -1,17 +1,19 @@
 export class Tag{
 
-    static readonly ANGULAR = new Tag ('Angular','blue');
-    static readonly HTML = new Tag ('HTML5','red');
-    static readonly CSS = new Tag ('CSS3','green');
-    static readonly PYTHON = new Tag ('Python','pink');
-    static readonly CSHARP = new Tag ('CSHARP','orange');
-    static readonly NODEJS = new Tag ('node.JS','brown');
-    static readonly ASPNET = new Tag ('ASP.NET','purple');
-    static readonly JAVASCRIPT = new Tag ('JavaScript','blue');
-    static readonly JAVA = new Tag ('Java','red');
-    static readonly TYPESCRIPT = new Tag ('TypeScript','pink');
-    static readonly SQL = new Tag ('SQL','pink');
-    static readonly DJANGO = new Tag ('DJANGO','blue');
+    static readonly ANGULAR = new Tag ('Angular','#B45541');
+    static readonly HTML = new Tag ('HTML5','#BCB147');
+    static readonly CSS = new Tag ('CSS3','#95C638');
+    static readonly PYTHON = new Tag ('PYTHON','#29A638');
+    static readonly CSHARP = new Tag ('C#','#3D5AAD');
+    static readonly NODEJS = new Tag ('NODE.JS','#7E5DD5');
+    static readonly ASPNET = new Tag ('ASP.NET','#9D57C8');
+    static readonly JAVASCRIPT = new Tag ('JAVASCRIPT','#AF5596');
+    static readonly JAVA = new Tag ('JAVA','#1984A0');
+    static readonly TYPESCRIPT = new Tag ('TypeScript','#0A6A05');
+    static readonly SQL = new Tag ('SQL','#0D5378');
+    static readonly DJANGO = new Tag ('DJANGO','#D48935');
+    static readonly BOOST = new Tag ('BOOTSTRAP','#E85519');
+    static readonly GIT = new Tag ('GIT','#024642');
     
     private constructor(private readonly key: string, public readonly color:string){
 
--- src/app/_services/projects.service.ts
+++ src/app/_services/projects.service.ts
@@ -9,20 +9,35 @@ export class ProjectsService {
   projecto: Projecto[] = [
     {
       id: 0,
-      nombre: 'Porftolio con Angular',
-      sumario: 'Actualizando usando Angular',
-      descripcion: '',
-      linkProyecto: '',
-      tags: [Tag.ANGULAR, Tag.HTML, Tag.CSS, Tag.TYPESCRIPT],
-      imagen: [],
+      nombre: 'Club Deportivo E1',
+      sumario: 'Proyecto integrador POO de tecnicatura primer año realizo con winform y mysql',
+      descripcion: `El proyecto integrador se desarrolló como parte de las asignaturas "Desarrollo de Sistemas Orientado a Objetos" y
+       "Modelado y Diseño de Software". La aplicación diseñada representa un ejercicio práctico de los conceptos aprendidos en estas materias, aplicando los pilares de la POO y diversos diagramas de modelado,
+       como diagramas de clases, casos de uso, secuencia y estados.
+        ` ,
+      linkProyecto: 'https://github.com/NicolasLLans/EspacioDeTrabajoPOO',
+      tags: [Tag.CSHARP, Tag.SQL,Tag.GIT],
+      imagen: ["../../assets/loginclub.png",
+      "../../assets/inicioclub.png",
+      "../../assets/inscribirclub.png",
+      "../../assets/altaactividad.png",
+      "../../assets/altaempleado.png",
+      ],
     },
     {
       id: 1,
       nombre: 'Juego Preguntas',
-      sumario: 'Actualizando usando Angular',
-      descripcion: 'Juego de preguntas realizado con Python y Django',
+      sumario: ' Realizado con Python y Djando como proyecto final del curso en Informatorio chaco',
+      descripcion:  `Una aplicación web hecha el Backend en Python3 con el framework
+      Django 3.2 en donde los usuarios pueden crear una cuenta loguearse y
+      poder unas preguntas precargadas por los administradores de la página
+      al responder todas las preguntas muestran un ranking general de los 10
+      usuarios que respondieron mejor las preguntas otorgando 10 puntos
+      por cada pregunta correcta. Además los administradores poseen su
+      página de administración donde pueden agregar, eliminar los temas y
+      las preguntas.`,
       linkProyecto: 'https://github.com/okijulian/Projectoinfo2021',
-      tags: [Tag.PYTHON, Tag.HTML, Tag.CSS],
+      tags: [Tag.PYTHON, Tag.HTML, Tag.CSS,Tag.SQL,Tag.DJANGO,Tag.BOOST,Tag.GIT],
       imagen: [
         "../../assets/project1.0.PNG",
         "../../assets/project1.1-administrador.PNG",
@@ -37,12 +52,20 @@ export class ProjectsService {
     },
     {
       id: 2,
-      nombre: 'Club Deportivo',
-      sumario: 'Actualizando usando Angular',
-      descripcion: '',
-      linkProyecto: '',
-      tags: [Tag.CSHARP],
-      imagen: [],
+      nombre: 'Gestion Bibilioteca',
+      sumario: 'Proyecto Final de Curso POO en JAVA realizado en consola',
+      descripcion:  `aplicación de gestión de
+      biblioteca en Java, utilizando conceptos de programación orientada a
+      objetos. La aplicación permitirá a los usuarios administrar el inventario de
+      libros de una biblioteca, registrar préstamos y devoluciones, y realizar
+      búsquedas en la colección de libros. `,
+      linkProyecto: 'https://github.com/okijulian/proyectoBiblioteca',
+      tags: [Tag.JAVA,Tag.GIT],
+      imagen: ["../../assets/menulibros.png",
+      "../../assets/menuprestamo.png",
+      "../../assets/menuprincial.png",
+      "../../assets/menuusuarios.png"
+      ],
     },
   ];
 
